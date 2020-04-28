4bfca4066b588f24814770c7ec6525003c85f11a
Andres Vazquez <andres@data99.com.ar>
2020-04-27 22:45:57 -0300

    fix test again

--- test/test_base.py
+++ test/test_base.py
@@ -1,6 +1,7 @@
 from pywisc.wisc_base import Wisc
 
 
-df = 'test/test_wisc_01.json'
-w = Wisc(definition_data=df)
-assert w.is_valid
+def test_01():
+    df = 'test/test_wisc_01.json'
+    w = Wisc(definition_data=df)
+    assert w.is_valid
